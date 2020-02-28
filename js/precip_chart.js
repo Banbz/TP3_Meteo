fd619fb731148c90a07424e64d998717f63af332
Axel <a.gallois@cs2i-bourgogne.com>
2020-02-28 15:16:45 +0100

    carte météo avec logo

--- js/precip_chart.js
+++ js/precip_chart.js
@@ -11,7 +11,7 @@ function loadJson(f) {
     data = f;
     var result = data.map(function (e) {
         return {
-            t: (e.t)/100,
+            p: e.p,
         }
     });
 
@@ -23,7 +23,7 @@ function loadJson(f) {
 
 // 6. Y scale will use the randomly generate number
     var yScale = d3.scaleLinear()
-        .domain([0, d3.max(result, dataPoint => dataPoint.t)]) // input
+        .domain([0, d3.max(result, dataPoint => dataPoint.p)]) // input
         .range([height, 0]); // output
 
 
@@ -34,10 +34,10 @@ function loadJson(f) {
         .curve(d3.curveMonotoneX) // apply smoothing to the line
 
 // 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
-    var dataset = d3.range(n).map(function(d) { return {"y": result[d]["t"] } });
+    var dataset = d3.range(n).map(function(d) { return {"y": result[d]["p"] } });
 
 // 1. Add the SVG to the page and employ #2
-    var svg = d3.select("#line_temp").append("svg")
+    var svg = d3.select("#line_precip").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -60,7 +60,7 @@ function loadJson(f) {
         .attr("class", "line") // Assign a class for styling
         .attr("d", line); // 11. Calls the line generator
 
-    var tool = d3.select("#wrapper").append("div")
+    var tool_precip = d3.select("#tooltip_work").append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
 
@@ -73,16 +73,16 @@ function loadJson(f) {
         .attr("cy", function(d) { return yScale(d.y) })
         .attr("r", 5)
         .on("mouseover", function(a, b, c) {
-            tool.transition()
+            tool_precip.transition()
                 .duration(200)
                 .style("opacity", .9);
-            tool.html("Température : " + a['y'] + " °C")
+            tool_precip.html("Précipitations : " + a['y'] + " mm")
                 .style("left", (d3.event.pageX + 30) + "px")
                 .style("top", (d3.event.pageY - 30) + "px")
         })
         .on("mouseout", function() {
-            tool.style("opacity", 0);
-            tool.html("")
+            tool_precip.style("opacity", 0);
+            tool_precip.html("")
                 .style("left", "-500px")
                 .style("top", "-500px");
         })
--- js/script.js
+++ js/script.js
@@ -1,38 +1,69 @@
-const width = 700, height = 700;
-const path = d3.geoPath();
+var data = [];
+var stations = [];
+var stationsName = [];
+const width = 750, height = 750;
+
 const projection = d3.geoConicConformal()
     .center([2.454071, 46.279229])
-    .scale(3500)
+    .scale(3800)
     .translate([width / 2, height / 2]);
-path.projection(projection);
 
-const svg = d3
-    .select("#map")
-    .append("svg")
-    .attr("id", "svg")
-    .attr("viewBox", `0 0 ${width} ${height}`)
-;
-const deps = svg.append("g");
-d3.json('./json/departments.json').then(function(geojson) {
-    deps.selectAll("path")
-        .data(geojson.features)
-        .enter()
-        .append("path")
-        .attr("fill", "#486DDA")
-        .attr("d", path);
-});
+function getDay(){
+    var selectPays = document.getElementById('select_ville');
+    var day = 1;
+    var meteoDate = $('#datepicker').datepicker('getDate');
+    if (meteoDate !== null) { // if any date selected in datepicker
+        meteoDate instanceof Date; // -> true
+        day = meteoDate.getDate();
+        meteoDate.getMonth();
+        meteoDate.getFullYear();
+    }
+    return day;
+}
 
-d3.json('./json/meteo.json').then(function(data) {
+function createMap() {
+
+    const path = d3.geoPath();
+
+    path.projection(projection);
+
+    const svg = d3.select('#map')
+        .append("svg")
+        .attr("id", "svg")
+        .attr("viewBox", "0 0 " + width * 1.15 + " " + height);
+
+    const deps = svg.append("g");
+
+    d3.json('./json/departments.json').then(function(geojson) {
+        deps.selectAll("path")
+            .data(geojson.features)
+            .enter()
+            .append("path")
+            .attr('class', 'department')
+            .attr('fill', '#486DDA')
+            .attr("d", path);
+    });
+}
+
+var request = d3.json("./json/meteo.json")
+    .then(loadJson).catch(console.error);
+
+function loadJson(d){
+    data = d;
+    createStation();
+    createMap();
+    createVille();
+    stationName();
+}
+
+function stationName()
+{
     var stations = data[0]["station"];
     var result = stations.map(function(e){
         return{
-            lat : e.lat,
-            lng : e.lat,
             name : e.n,
         }
-    })
-    console.log(result);
-
+    });
     let dropdown = $('#station_select');
 
     dropdown.empty();
@@ -43,29 +74,153 @@ d3.json('./json/meteo.json').then(function(data) {
     $.each(result, function (key, entry) {
         dropdown.append($('<option></option>').attr('value', entry.name).text(entry.name));
     });
-});
+}
 
+function createStation(){
+    for(var i = 0; i < data.length; i++)
+    {
+        for(var j = 0; j < data[i]['station'].length; j++)
+        {
+            if(!stationsName.includes(data[i]['station'][j]['n']))
+            {
+                stationsName.push(data[i]['station'][j]['n']);
+                var nom = data[i]['station'][j]['n'];
+                var latitude = data[i]['station'][j]['lat'];
+                var longitude = data[i]['station'][j]['lng'];
 
-var div = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
+                var detailJour = [];
+                for(var l = 0; l < data.length; l++)
+                {
+                    for(var m = 0; m < data[l]['station'].length; m++)
+                    {
+                        if(data[l]['station'][m]['n'] == nom){
 
-d3.json('./json/departments.json').then(function(geojson) {
-    deps.selectAll("path")
-        .data(geojson.features)
-        .enter()
-        .append("path")
-        .attr('class', 'department')
-        .attr("fill", "#486DDA")
-        .attr("d", path)
+                            var temp = data[l]['station'][m]['t'];
+                            var TempMoy = parseFloat(temp/100).toFixed(0);
+                            var PreciMoy = parseFloat(data[l]['station'][m]['p']).toFixed(2);
+                            var min = 0;
+                            var max = 0;
+
+                            for(var z = 0; z < data[l]['station'][m]['hours'].length; z++)
+                            {
+                                if(z == 0)
+                                {
+                                    max = data[l]['station'][m]['hours'][z]['t']/100;
+                                    min = data[l]['station'][m]['hours'][z]['t']/100;
+                                }
+                                if(data[l]['station'][m]['hours'][z]['t']/100 > max)
+                                {
+                                    max = data[l]['station'][m]['hours'][z]['t']/100;
+                                }
+                                if(data[l]['station'][m]['hours'][z]['t']/100 < min) {
+                                    min = data[l]['station'][m]['hours'][z]['t']/100;
+                                }
+                            }
+
+                            var infoMeteo = {
+                                TempMoy : TempMoy,
+                                PreciMoy : PreciMoy,
+                                min : min,
+                                max : max,
+                                detailHeure : data[l]['station'][m]['hours']
+                            }
+                            detailJour.push(infoMeteo);
+                        }
+                    }
+                }
+                var infoStation = {
+                    nom: nom,
+                    latitude: latitude,
+                    longitude : longitude,
+                    detailJour : detailJour
+                };
+
+                stations.push(infoStation);
+            }
+        }
+    }
+}
+
+function updateVille()
+{
+    day = getDay();
+    d3.selectAll("svg image")    .attr("xlink:href", function (d) {
+        if(d.detailJour.length >= day){
+            if(d.detailJour[day - 1]['TempMoy'] <= 1 && d.detailJour[day - 1]['PreciMoy'] >= 0.75 && d.detailJour[day - 1]['TempMoy'] >= -3)
+                return "./img/snow.svg";
+            else if( d.detailJour[day - 1]['TempMoy'] >= 25 && d.detailJour[day - 1]['PreciMoy'] <= 0.15)
+                return "./img/sun.svg";
+            else if(d.detailJour[day - 1]['PreciMoy'] >= 0.50)
+                return "./img/rain.svg";
+            else if(d.detailJour[day - 1]['PreciMoy'] >= 0.25 )
+                return "./img/cloud-sun.svg";
+            else if(d.detailJour[day - 1]['PreciMoy'] > 0.00 && d.detailJour[day - 1]['TempMoy'] <= 15)
+                return "./img/cloud.svg";
+            else if(d.detailJour[day - 1]['PreciMoy'] == 0.00)
+                return "./img/sun.svg";
+        }
+    })
+
+    d3.selectAll("#map svg text").text(function(d){
+        try {
+            if(d.detailJour.length > day - 1)
+            {
+                return d.detailJour[day - 1]['TempMoy'] + "°C";
+            }
+        }
+        catch(error) {
+        }
+    });
+}
+
+function createVille() {
+
+    day = getDay();
+
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0)
+        .style("left", "-500px")
+        .style("top", "-500px");
+
+    var svg = d3.select("#map").select("svg");
+
+    var elem = svg.selectAll("g")
+        .data(stations);
+
+    var elemEnter = elem.enter()
+        .append("g");
+
+    var circle = elemEnter.append("image")
+        .attr("xlink:href", function (d) {
+            if(d.detailJour.length >= day){
+                if(d.detailJour[day - 1]['TempMoy'] <= 1 && d.detailJour[day - 1]['PreciMoy'] >= 0.75 && d.detailJour[day - 1]['TempMoy'] >= -1)
+                    return "./img/snow.svg";
+                else if( d.detailJour[day - 1]['TempMoy'] >= 20 && d.detailJour[day - 1]['PreciMoy'] <= 0.2)
+                    return "./img/sun.svg";
+                else if(d.detailJour[day - 1]['PreciMoy'] >= 1)
+                    return "./img/rain.svg";
+                else if(d.detailJour[day - 1]['PreciMoy'] >= 0.25 )
+                    return "./img/cloud-sun.svg";
+                else if(d.detailJour[day - 1]['PreciMoy'] > 0.00 && d.detailJour[day - 1]['TempMoy'] <= 15)
+                    return "./img/cloud.svg";
+                else if(d.detailJour[day - 1]['PreciMoy'] == 0.00)
+                    return "./img/sun.svg";
+            }
+        })
+        .attr("x", function (d) { return projection([d.longitude, d.latitude])[0] - 15; })
+        .attr("y", function (d) { return projection([d.longitude, d.latitude])[1] - 15; })
+        .attr("width", "40px")
         .on("mouseover", function(d) {
             div.transition()
                 .duration(200)
                 .style("opacity", .9);
-            div.html("Département : " + d.properties.NOM_DEPT + "<br/>"
-                +  "Région : " + d.properties.NOM_REGION)
-                .style("left", (d3.event.pageX + 30) + "px")
-                .style("top", (d3.event.pageY - 30) + "px")
+            div.html("Station de " + d.nom+ "<br/>"
+                + " T° mini du jour : " + d.detailJour[day - 1]['min'] + "°C</br>"
+                + "T° maxi du jour : " + d.detailJour[day - 1]['max'] + "°C</br>"
+                + "Précipitation du jour : " + d.detailJour[day - 1]['PreciMoy'] + "mm</br>")
+                .style("left", (d3.event.pageX - 10) + "px")
+                .style("top", (d3.event.pageY - 10) + "px");
         })
         .on("mouseout", function(d) {
             div.style("opacity", 0);
@@ -73,9 +228,4 @@ d3.json('./json/departments.json').then(function(geojson) {
                 .style("left", "-500px")
                 .style("top", "-500px");
         });
-});
-
-
-
-
-
+}
